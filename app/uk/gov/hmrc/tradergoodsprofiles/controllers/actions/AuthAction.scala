a2fe8f09fbe9fc7c14ef7f68ce418e336406427c
mauro-fanton <mauro.fanton@opencastsoftware.com>
2024-04-16 08:28:21 +0100

    first implementation

---
+++ app/uk/gov/hmrc/tradergoodsprofiles/controllers/actions/AuthAction.scala
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.tradergoodsprofiles.controllers.actions
+
+import com.google.inject.ImplementedBy
+import play.api.Logging
+import play.api.mvc._
+import uk.gov.hmrc.auth.core.{AuthConnector, AuthorisedFunctions}
+import uk.gov.hmrc.play.bootstrap.backend.controller.BackendController
+import uk.gov.hmrc.tradergoodsprofiles.models.auth.EnrolmentRequest
+
+import javax.inject.Inject
+import scala.concurrent.{ExecutionContext, Future}
+
+
+class AuthActionImpl @Inject()
+(
+  override val authConnector: AuthConnector,
+  cc: ControllerComponents,
+  val parser: BodyParsers.Default
+)
+(implicit val ec: ExecutionContext)
+  extends BackendController(cc) with AuthorisedFunctions with AuthAction with Logging {
+
+  override def invokeBlock[A](request: Request[A], block: EnrolmentRequest[A] => Future[Result]): Future[Result] = ???
+
+  override protected def executionContext: ExecutionContext = ???
+}
+
+@ImplementedBy(classOf[AuthActionImpl])
+trait AuthAction extends ActionBuilder[EnrolmentRequest, AnyContent] with ActionFunction[Request, EnrolmentRequest]{
+
+}
---
+++ test/uk/gov/hmrc/tradergoodsprofiles/controllers/actions/AuthActionSpec.scala
@@ -0,0 +1,13 @@
+package uk.gov.hmrc.tradergoodsprofiles.controllers.actions
+
+import org.scalatestplus.play.PlaySpec
+
+class AuthActionSpec extends PlaySpec {
+
+  "authorisation" should {
+    "authorise an enrolment" in {
+
+      val authenticator = new AuthActionImpl()
+    }
+  }
+}
