7b358d341a32504d4489f22a993d33cc2da6b8cf
Ben Healy <139165148+taylors-version@users.noreply.github.com>
2024-04-26 11:33:52 +0100

    TGP-711 Adding scalatest dependency

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -9,8 +9,9 @@ object AppDependencies {
   )
 
   val test = Seq(
-    "uk.gov.hmrc" %% "bootstrap-test-play-30" % bootstrapVersion,
-    "org.mockito" %% "mockito-scala"          % "1.17.31"
+    "uk.gov.hmrc"       %% "bootstrap-test-play-30" % bootstrapVersion,
+    "org.mockito"       %% "mockito-scala"          % "1.17.31",
+    "org.scalatestplus" %% "scalacheck-1-17"        % "3.2.17.0"
   ).map(_ % Test)
 
   val it   = Seq.empty
