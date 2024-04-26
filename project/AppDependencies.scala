ec412ef4e95fd02d1099a0f1dacc2574a5ac8c84
Ben Healy <139165148+taylors-version@users.noreply.github.com>
2024-04-26 11:50:33 +0100

    TGP-711 remove scalatestplus dependency

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -9,9 +9,8 @@ object AppDependencies {
   )
 
   val test = Seq(
-    "uk.gov.hmrc"       %% "bootstrap-test-play-30" % bootstrapVersion,
-    "org.mockito"       %% "mockito-scala"          % "1.17.31",
-    "org.scalatestplus" %% "scalacheck-1-17"        % "3.2.17.0"
+    "uk.gov.hmrc" %% "bootstrap-test-play-30" % bootstrapVersion,
+    "org.mockito" %% "mockito-scala"          % "1.17.31"
   ).map(_ % Test)
 
   val it   = Seq.empty
