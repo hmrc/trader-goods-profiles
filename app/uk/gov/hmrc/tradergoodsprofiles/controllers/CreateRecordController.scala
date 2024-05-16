26382872fa7cfc3bd89ecc281d966d52b9cbd396
IoannisLazaridis-oc <128503072+IoannisLazaridis-oc@users.noreply.github.com>
2024-05-16 18:43:50 +0100

    TGP-377: Implemented basic  controller for create record endpoint

---
+++ app/uk/gov/hmrc/tradergoodsprofiles/controllers/CreateRecordController.scala
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.tradergoodsprofiles.controllers
+
+import play.api.libs.json.{JsValue, Json}
+import play.api.mvc.{Action, ControllerComponents}
+import uk.gov.hmrc.play.bootstrap.backend.controller.BackendController
+import uk.gov.hmrc.tradergoodsprofiles.controllers.actions.{AuthAction, ValidateHeaderAction}
+import uk.gov.hmrc.tradergoodsprofiles.models.CreateRecordRequest
+import uk.gov.hmrc.tradergoodsprofiles.services.{DateTimeService, RouterService}
+
+import javax.inject.{Inject, Singleton}
+import scala.concurrent.{ExecutionContext, Future}
+
+@Singleton
+class CreateRecordController @Inject() (
+  authAction: AuthAction,
+  validateHeaderAction: ValidateHeaderAction,
+  dateTimeService: DateTimeService,
+  routerService: RouterService,
+  cc: ControllerComponents
+)(implicit ec: ExecutionContext)
+    extends BackendController(cc) {
+
+  def createRecord(eori: String): Action[JsValue] =
+    (authAction(eori) andThen validateHeaderAction).async(parse.json) { implicit request =>
+      request.body
+        .validate[CreateRecordRequest]
+        .fold(
+          errors => {
+            val errorMessages = errors.map { case (path, validationErrors) =>
+              path.toJsonString -> validationErrors.map(_.message).mkString(", ")
+            }.toMap
+            Future.successful(BadRequest(Json.obj("error" -> "Invalid JSON", "details" -> errorMessages)))
+          },
+          createRequest =>
+            routerService.createRecord(eori, createRequest).value.map {
+              case Right(recordResponse) => Created(Json.toJson(recordResponse))
+              case Left(errorResult)     => errorResult
+            }
+        )
+    }
+}
