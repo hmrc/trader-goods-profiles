d7bef956ee5f655e6a35a26c78ff16431ed441bb
IoannisLazaridis-oc <128503072+IoannisLazaridis-oc@users.noreply.github.com>
2024-06-12 10:31:20 +0100

    TGP-1315: Improved RouterConnector url building with query params

--- app/uk/gov/hmrc/tradergoodsprofiles/connectors/RouterConnector.scala
+++ app/uk/gov/hmrc/tradergoodsprofiles/connectors/RouterConnector.scala
@@ -17,7 +17,7 @@
 package uk.gov.hmrc.tradergoodsprofiles.connectors
 
 import com.codahale.metrics.MetricRegistry
-import io.lemonlabs.uri.UrlPath
+import io.lemonlabs.uri.{QueryString, Url, UrlPath}
 import play.api.Logging
 import play.api.http.{HeaderNames, MimeTypes}
 import play.api.libs.json.Json
@@ -87,7 +87,7 @@ class RouterConnector @Inject() (
 
   def removeRecord(eori: String, recordId: String, actorId: String)(implicit hc: HeaderCarrier): Future[HttpResponse] =
     withMetricsTimerAsync("tgp.removerecord.connector") { _ =>
-      val url = appConfig.routerUrl.toUrl + routerRouteRemoveRecord(eori, recordId, actorId)
+      val url = routerRouteRemoveRecord(eori, recordId, actorId)
 
       httpClient
         .delete(url"$url")
@@ -143,10 +143,10 @@ class RouterConnector @Inject() (
       s"$routerBaseRoute/$eoriNumber/records/$recordId"
     )
 
-  private def routerRouteRemoveRecord(eoriNumber: String, recordId: String, actorId: String): String = {
-    val uri = uri"$routerBaseRoute/$eoriNumber/records/$recordId?actorId=$actorId"
-    s"$uri"
-  }
+  private def routerRouteRemoveRecord(eoriNumber: String, recordId: String, actorId: String): Url =
+    appConfig.routerUrl
+      .withPath(UrlPath.parse(s"$routerBaseRoute/$eoriNumber/records/$recordId"))
+      .withQueryString(QueryString.fromPairs("actorId" -> actorId))
 
   private def routerRouteGetRecords(
     eoriNumber: String,
