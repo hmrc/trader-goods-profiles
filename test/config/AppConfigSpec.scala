0527d570bf1a124bee7d4a39dda3eedbb57d3fea
IoannisLazaridis-oc <128503072+IoannisLazaridis-oc@users.noreply.github.com>
2024-09-09 11:47:30 +0100

    TGP-2364: Remove drop_1_1_enabled flag

--- test/config/AppConfigSpec.scala
+++ test/config/AppConfigSpec.scala
@@ -27,7 +27,7 @@ class AppConfigSpec extends PlaySpec {
   private val validAppConfig =
     """
       |appName=trader-goods-profiles
-      |feature.drop_1_1_enabled=true
+      |feature.clientIdHeaderDisabled=true
     """.stripMargin
 
   private def createAppConfig(configSettings: String) = {
