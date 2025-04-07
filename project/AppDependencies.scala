ce66e4f0c45f7f9f5430367b2417db043a59a5de
Reece-Carruthers <99188015+Reece-Carruthers@users.noreply.github.com>
2025-04-07 14:12:08 +0100

    TGP-3281: Dependancies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,22 +2,22 @@ import sbt._
 
 object AppDependencies {
 
-  private val bootstrapVersion = "9.7.0"
-  private val catsVersion      = "2.6.1"
+  private val bootstrapVersion = "9.11.0"
+  private val catsVersion      = "2.13.0"
 
-  val compile = Seq(
+  val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"   %% "bootstrap-backend-play-30" % bootstrapVersion,
     "org.typelevel" %% "cats-core"                 % catsVersion,
     "io.lemonlabs"  %% "scala-uri"                 % "4.0.3",
-    "com.beachape"     %% "enumeratum-play"           % "1.8.0"
+    "com.beachape"     %% "enumeratum-play"           % "1.8.2"
   )
 
-  val test = Seq(
+  val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"         %% "bootstrap-test-play-30" % bootstrapVersion,
-    "org.mockito"         %% "mockito-scala"          % "1.17.31",
+    "org.mockito"         %% "mockito-scala"          % "1.17.37",
     "org.typelevel"       %% "cats-core"              % catsVersion,
-    "io.swagger.parser.v3" % "swagger-parser-v3"      % "2.1.14"
+    "io.swagger.parser.v3" % "swagger-parser-v3"      % "2.1.26"
   ).map(_ % Test)
 
-  val it   = Seq.empty
+  val it: Seq[Nothing] = Seq.empty
 }
